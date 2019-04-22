5ad6035a91c1eb3d33556ddfee25b99ba18ee431
Lou <louschlessinger96@gmail.com>
2019-04-22 02:11:14 -0400

    Add BOMS grammar traversal

--- model_spaces/boms_grammar_traversal.py
+++ model_spaces/boms_grammar_traversal.py
@@ -1,5 +1,7 @@
 from typing import List
 
+import numpy as np
+
 from model_spaces.core.covariance import Covariance
 from model_spaces.core.covariance_grammar import CovarianceGrammar
 from model_spaces.core.gp_model import GPModel
@@ -20,11 +22,47 @@ class BomsGrammarTraversal(CovarianceGrammar):
         self.number_of_top_k_best = 3
         self.number_of_random_walks = 15
 
-    def get_candidates(self, selected_models: GPModel, fitness_score: float) -> List[Covariance]:
-        pass
+    def get_candidates(self, selected_models: List[GPModel], fitness_score: List[float]) -> List[Covariance]:
+        # exploration
+        total_num_walks = self.number_of_random_walks
+        candidates_random = self.expand_random(total_num_walks)
+
+        # exploitation
+        candidates_best = self.expand_best(selected_models, fitness_score)
+
+        # concatenate
+        candidates = candidates_best + candidates_random
+        return candidates
 
     def expand_random(self, total_num_walks: int) -> List[Covariance]:
-        pass
+        parameter = self.random_walk_geometric_dist_parameter
+        new_kernels = []
+        for i in range(total_num_walks):
+            frontier = self.base_kernels
+            depth = np.random.geometric(parameter)
+            new_kernel = None
+            while depth >= 0:
+                new_kernel = np.random.choice(frontier)
+                frontier = self.expand(new_kernel)
+                depth -= 1
+            new_kernels.append(new_kernel)
 
-    def expand_best(self, selected_models: GPModel, fitness_score: float) -> List[Covariance]:
-        pass
+        return new_kernels
+
+    def expand_best(self, selected_models: List[GPModel], fitness_score: List[float]) -> List[Covariance]:
+        new_kernels = []
+        num_exploit_top = self.number_of_top_k_best
+        if len(fitness_score) < 2:
+            return new_kernels
+
+        indices = np.argsort(fitness_score)[::-1]
+        last_index = min(num_exploit_top, len(fitness_score))
+        indices = indices[:last_index]
+
+        models_with_highest_score = [selected_models[i] for i in indices]
+        for model in models_with_highest_score:
+            kernel_to_expand = model.covariance
+            new_kernel_list = self.expand(kernel_to_expand)
+            new_kernels += new_kernel_list
+
+        return new_kernels
